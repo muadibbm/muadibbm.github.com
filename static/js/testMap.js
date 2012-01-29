a87726039dfb89cba57e4dcf6a1279cfe40418b1
Mehrdad Dehdashti <muadibbm@yahoo.com>
2012-01-29 01:31:30 -0800

    trying to fix the bug in propagation

--- static/js/testMap.js
+++ static/js/testMap.js
@@ -9,9 +9,9 @@
 
   root.parseMap = function(string) {
     var char, firstRow, grass, height, map, s, tree, water, width, _ref;
-    tree = new CellType(true, "static/images/tree.png", 10);
-    grass = new CellType(false, "static/images/grass.png", -1);
-    water = new CellType(false, "static/images/water.png", -1);
+    root.tree = new CellType(true, "static/images/tree.png", 10);
+    root.grass = new CellType(false, "static/images/grass.png", -1);
+    root.water = new CellType(false, "static/images/water.png", -1);
     width = 0;
     firstRow = true;
     height = 0;
@@ -19,11 +19,11 @@
     for (s = 0, _ref = string.length - 1; 0 <= _ref ? s <= _ref : s >= _ref; 0 <= _ref ? s++ : s--) {
       char = string[s];
       if (char === '0') {
-        map.push(new Cell(grass));
+        map.push(new Cell(root.grass));
       } else if (char === '1') {
-        map.push(new Cell(tree));
+        map.push(new Cell(root.tree));
       } else if (char === '2') {
-        map.push(new Cell(water));
+        map.push(new Cell(root.water));
       } else if (char === '$') {
         firstRow = false;
         height = height + 1;
