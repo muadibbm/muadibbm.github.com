1ac0e1d5bbccdb04db739e0b68cf565ce1a6eef9
muadibbm <muadibbm@yahoo.com>
2018-09-27 12:01:02 -0400

    projects updated

--- send-mail.php
+++ send-mail.php
@@ -15,7 +15,6 @@ $from = $_POST['email'];
 //data
 $msg = "NAME: "  .$_POST['name']    ."<br>\n";
 $msg .= "EMAIL: "  .$_POST['email']    ."<br>\n";
-$msg .= "WEBSITE: "  .$_POST['web']    ."<br>\n";
 $msg .= "COMMENTS: "  .$_POST['comments']    ."<br>\n";
 
 //Headers
