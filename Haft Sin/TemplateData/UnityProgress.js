8ee87f89c943006f9b90fe42f82f16032937108d
mehrdad <mehrdad@artifact5.com>
2021-03-18 22:44:52 -0400

    fixed issue with progress bar

--- Haft Sin/TemplateData/UnityProgress.js
+++ Haft Sin/TemplateData/UnityProgress.js
@@ -21,6 +21,6 @@ function UnityProgress(unityInstance, progress) {
   }
   unityInstance.progress.full.style.width = (100 * progress) + "%";
   unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  //if (progress == 1)
-    //unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+  if (progress == 1)
+    unityInstance.progress.style.display = "none"; // unityInstance.logo.style.display = 
 }
