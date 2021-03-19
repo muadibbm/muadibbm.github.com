3a07aa45ada2da1969dfdf9d380dd12384138220
mehrdad <mehrdad@artifact5.com>
2021-03-18 22:28:56 -0400

    removed the unity logo from loading screen

--- Haft Sin/TemplateData/UnityProgress.js
+++ Haft Sin/TemplateData/UnityProgress.js
@@ -1,11 +1,13 @@
 function UnityProgress(unityInstance, progress) {
   if (!unityInstance.Module)
     return;
+  /*
   if (!unityInstance.logo) {
     unityInstance.logo = document.createElement("div");
     unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
     unityInstance.container.appendChild(unityInstance.logo);
   }
+  */
   if (!unityInstance.progress) {    
     unityInstance.progress = document.createElement("div");
     unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
@@ -19,6 +21,6 @@ function UnityProgress(unityInstance, progress) {
   }
   unityInstance.progress.full.style.width = (100 * progress) + "%";
   unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
-  if (progress == 1)
-    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+  //if (progress == 1)
+    //unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
 }
